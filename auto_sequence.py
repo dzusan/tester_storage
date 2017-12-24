093787072db1003ea342a5f3b5062b937504839d
Sergey Lenkov <lenkov@lenkov.work>
2017-12-24 11:40:37 +0300

    Reallocated functions + refactored

--- auto_sequence.py
+++ auto_sequence.py
@@ -1,67 +1,7 @@
 import sys
 import time
-from datetime import datetime
 import psutil
-from startup_meter import *
-
-def progressMarker(flag):
-    if flag[0] == True:
-        sys.stdout.write("\b|")
-        flag[0] = False
-    else:
-        sys.stdout.write("\b-")
-        flag[0] = True
-    sys.stdout.flush()
-
-
-def ConditionLog():
-
-    logsDir = "Condition logs"
-    if not os.path.exists(logsDir):
-        os.makedirs(logsDir)
-
-    nowTimeStamp = datetime.now().strftime("%Y-%m-%d %H.%M.%S")
-
-    try:
-        logFile = open(logsDir + "/condition from " + nowTimeStamp + ".log", "w")
-    except Exception as e:
-        print("ERROR opening log file: {}".format(e))
-    else:
-        print("{} log file opened successfully".format(logFile.name))
-
-    logFile.write("Automated test sequence from " + nowTimeStamp + ". Initial system conditions.\n")
-
-    initialTime = time.time()
-    print("Processing condition log -", end="")
-    markerFlag = [True]
-    cpuUsage = 0
-
-    for i in range(10):
-        cpuUsage += psutil.cpu_percent(interval=0.5) / 10
-        progressMarker(markerFlag)
-
-    logFile.write("CPU Usage average: {}%\n".format(cpuUsage))
-    logFile.write("CPU Usage: {}\n".format(psutil.cpu_times()))
-    logFile.write("MEM Usage: {}\n".format(psutil.virtual_memory()))
-
-    for proc in psutil.process_iter():
-        logFile.write("PID {:>5d} | {:>40} | Created {} | CPU {:5.1f}% | MEM {:5.1f}%\n".format(
-            proc.pid,
-            proc.name(),
-            time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(proc.create_time())),
-            proc.cpu_percent(interval=0.5),
-            proc.memory_percent()
-            ))
-        progressMarker(markerFlag)
-
-    logFile.write("Logging successfully finished in: {}\n".format(time.strftime("%M minutes %S seconds", time.gmtime(time.time() - initialTime))))
-
-    try:
-        logFile.close()
-    except Exception as e:
-        print("\nERROR closing log file {}\n Log hasn't been recorded".format(logFile.name))
-    else:
-        print("\n{} has been recorded successfully".format(logFile.name))
+from auto_tests import *
 
 ConditionLog()
 
---
+++ auto_tests.py
@@ -0,0 +1,107 @@
+import psutil
+import time
+import os
+import sys
+import subprocess
+from datetime import datetime
+
+
+def progressMarker(flag):
+    if flag[0] == True:
+        sys.stdout.write("\b|")
+        flag[0] = False
+    else:
+        sys.stdout.write("\b-")
+        flag[0] = True
+    sys.stdout.flush()
+
+
+def ConditionLog():
+    logsDir = "Condition logs"
+    if not os.path.exists(logsDir):
+        os.makedirs(logsDir)
+
+    nowTimeStamp = datetime.now().strftime("%Y-%m-%d %H.%M.%S")
+
+    try:
+        logFile = open(logsDir + "/condition from " + nowTimeStamp + ".log", "w")
+    except Exception as e:
+        print("ERROR opening log file: {}".format(e))
+    else:
+        print("{} log file opened successfully".format(logFile.name))
+
+    logFile.write("Automated test sequence from " + nowTimeStamp + ". Initial system conditions.\n")
+
+    initialTime = time.time()
+    print("Processing condition log -", end="")
+    markerFlag = [True]
+    cpuUsage = 0
+
+    for i in range(10):
+        cpuUsage += psutil.cpu_percent(interval=0.5) / 10
+        progressMarker(markerFlag)
+
+    logFile.write("CPU Usage average: {}%\n".format(cpuUsage))
+    logFile.write("CPU Usage: {}\n".format(psutil.cpu_times()))
+    logFile.write("MEM Usage: {}\n".format(psutil.virtual_memory()))
+
+    for proc in psutil.process_iter():
+        logFile.write("PID {:>5d} | {:>40} | Created {} | CPU {:5.1f}% | MEM {:5.1f}%\n".format(
+            proc.pid,
+            proc.name(),
+            time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(proc.create_time())),
+            proc.cpu_percent(interval=0.5),
+            proc.memory_percent()
+            ))
+        progressMarker(markerFlag)
+
+    logFile.write("Logging successfully finished in: {}\n".format(time.strftime("%M minutes %S seconds", time.gmtime(time.time() - initialTime))))
+
+    try:
+        logFile.close()
+    except Exception as e:
+        print("\nERROR closing log file {}\n Log hasn't been recorded".format(logFile.name))
+    else:
+        print("\n{} has been recorded successfully".format(logFile.name))
+
+
+def startupMeter(progPath, threshold = 10, samplePeriod = 0.5, frameSize = 10):
+    # Init
+    frame = [threshold for x in range(frameSize)]
+    print("Starting up ", end="")
+    markerFlag = [True]
+
+    # Start timer
+    initial_time = time.time()
+
+    # Create process
+    proc = subprocess.Popen(progPath)
+    time.sleep(1)
+    meter = psutil.Process(proc.pid)
+    print(meter.name(), " -", end="")    
+
+    while True:
+        progressMarker(markerFlag)
+
+        try:
+            frame.append(meter.cpu_percent(interval=samplePeriod))
+        except Exception:
+            print("\nAny processes killed")
+            sys.exit()
+
+        avg = 0
+        for i in range(1, frameSize + 1):
+            avg += frame[-i] / frameSize
+
+        if avg < threshold:
+            finish_time = time.time()
+            overall = finish_time - initial_time - ((frameSize / 2) * samplePeriod)
+            print("\nOverall startup time ", overall)
+            proc.kill()
+            return overall
+
+# Usage
+# 
+# ConditionLog()
+# xxx = startupMeter("E:\\portable\\pro\\Altium\\AD17\\DXP.EXE")
+# print(xxx)
